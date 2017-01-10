670d72d56bb25720c931f6c6a0e60df2196cff05
Mounika <mounika@unmetric.com>
2017-01-10 16:53:28 +0530

    Prints HelloWorld

---
+++ Learning/src/com/learing/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.learing;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.err.println("Hello World!!");
+		
+	}
+
+}
